644e9bd148ab8de1390387fce30186bfc60e7cd0
Luke Bodwell <ljbodwell@wpi.edu>
2022-04-05 16:04:41 -0400

    Minor tweaks

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const PORT = 5000;
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-  res.redirect("/");
-});
+app.get("*", (req, res) => res.redirect("/"));
 
 app.listen(process.env.port || PORT, () => console.log("Listening on port " + PORT)); 
